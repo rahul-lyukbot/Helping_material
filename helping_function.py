849b544093176951d916f1148aefe85c7f171714
Coding_Rythm <101165838+rahul-lyukbot@users.noreply.github.com>
2022-06-14 14:29:17 +0530

    Update helping_function.py

--- helping_function.py
+++ helping_function.py
@@ -1,300 +1,289 @@
-# This functiion contain all helping function i need in Deep Learning with tensorflow
+### We create a bunch of helpful functions throughout the course.
+### Storing them here so they're easily accessible.
+
 import tensorflow as tf
 
+# Create a function to import an image and resize it to be able to be used with our model
+def load_and_prep_image(filename, img_shape=224, scale=True):
+  """
+  Reads in an image from filename, turns it into a tensor and reshapes into
+  (224, 224, 3).
 
-# Function 1 - Confusion matrix
+  Parameters
+  ----------
+  filename (str): string filename of target image
+  img_shape (int): size to resize target image to, default 224
+  scale (bool): whether to scale pixel values to range(0, 1), default True
+  """
+  # Read in the image
+  img = tf.io.read_file(filename)
+  # Decode it into a tensor
+  img = tf.image.decode_jpeg(img)
+  # Resize the image
+  img = tf.image.resize(img, [img_shape, img_shape])
+  if scale:
+    # Rescale the image (get all values between 0 and 1)
+    return img/255.
+  else:
+    return img
+
+# Note: The following confusion matrix code is a remix of Scikit-Learn's 
+# plot_confusion_matrix function - https://scikit-learn.org/stable/modules/generated/sklearn.metrics.plot_confusion_matrix.html
 import itertools
+import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.metrics import confusion_matrix
-def con_matrix(Y_true, Y-pred, figsize=(10, 8), classes, text_size = 10):
-  """
-   :params:
-          Y_true - it's contain the orginal values of Y.
-          Y_pred - it's contain the prediction made by our model.
-          figsize - contain the figure size by default is (10, 8) but you can set it.
-          classes - list of classes you have 
-          text_size - it contain the size of text to use 
-   :return:
-          it's return  a confusion matrix between true values of Y and the prediction our model make
-  """
-  cm = confusion_matrix(Y_true, Y_pred)
-    # convort our confusion_matrix into normalization form
-  cm_norm = cm.astype("float")/ cm.sum(axis=1)[:, np.newaxis]
-  n_classes = cm.shape[0]
 
-  # let's pretify it's
+# Our function needs a different name to sklearn's plot_confusion_matrix
+def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_size=15, norm=False, savefig=False): 
+  """Makes a labelled confusion matrix comparing predictions and ground truth labels.
+
+  If classes is passed, confusion matrix will be labelled, if not, integer class values
+  will be used.
+
+  Args:
+    y_true: Array of truth labels (must be same shape as y_pred).
+    y_pred: Array of predicted labels (must be same shape as y_true).
+    classes: Array of class labels (e.g. string form). If `None`, integer labels are used.
+    figsize: Size of output figure (default=(10, 10)).
+    text_size: Size of output figure text (default=15).
+    norm: normalize values or not (default=False).
+    savefig: save confusion matrix to file (default=False).
+  
+  Returns:
+    A labelled confusion matrix plot comparing y_true and y_pred.
+
+  Example usage:
+    make_confusion_matrix(y_true=test_labels, # ground truth test labels
+                          y_pred=y_preds, # predicted labels
+                          classes=class_names, # array of class label names
+                          figsize=(15, 15),
+                          text_size=10)
+  """  
+  # Create the confustion matrix
+  cm = confusion_matrix(y_true, y_pred)
+  cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis] # normalize it
+  n_classes = cm.shape[0] # find the number of classes we're dealing with
+
+  # Plot the figure and make it pretty
   fig, ax = plt.subplots(figsize=figsize)
-  # Create matrix plot
-  cax = ax.matshow(cm, cmap = plt.cm.Blues)
+  cax = ax.matshow(cm, cmap=plt.cm.Blues) # colors will represent how 'correct' a class is, darker == better
+  fig.colorbar(cax)
 
-  # create classes as labels
+  # Are there a list of classes?
   if classes:
     labels = classes
   else:
     labels = np.arange(cm.shape[0])
-
+  
   # Label the axes
-  ax.set(title = "Confusion matrix",
-        xlabel = "Predicted_Model",
-        ylabel = "True_label",
-        xticks = np.arange(n_classes),
-        yticks = np.arange(n_classes),
-        xticklabels = labels,
-        yticklabels = labels
-
-      
-  )  
-
-  # Set the xlabels to the bottom
+  ax.set(title="Confusion Matrix",
+         xlabel="Predicted label",
+         ylabel="True label",
+         xticks=np.arange(n_classes), # create enough axis slots for each class
+         yticks=np.arange(n_classes), 
+         xticklabels=labels, # axes will labeled with class names (if they exist) or ints
+         yticklabels=labels)
+  
+  # Make x-axis labels appear on bottom
   ax.xaxis.set_label_position("bottom")
   ax.xaxis.tick_bottom()
 
-  # increase the size of labels
-  ax.yaxis.label.set_size(text_size)
-  ax.xaxis.label.set_size(text_size)
-  ax.title.set_size(text_size)
-
-  # Setting the threshold for different colours
-  threshold = (cm.max() + cm.min())/2.
+  # Set the threshold for different colors
+  threshold = (cm.max() + cm.min()) / 2.
 
   # Plot the text on each cell
-  for i,j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-    plt.text(j, i, f"{cm[i,j]} ({cm_norm[i,j]*100:.1f}%)",
-    horizontalalignment="center",
-    color="white" if cm[i,j] > threshold else "black",
-     size=text_size)
-    
-    
-# Function 2 - Plot Decision Boundaries
-import matplotlib.pyplot as plt
-def plot_decision_boundaries(model, x, y):
+  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    if norm:
+      plt.text(j, i, f"{cm[i, j]} ({cm_norm[i, j]*100:.1f}%)",
+              horizontalalignment="center",
+              color="white" if cm[i, j] > threshold else "black",
+              size=text_size)
+    else:
+      plt.text(j, i, f"{cm[i, j]}",
+              horizontalalignment="center",
+              color="white" if cm[i, j] > threshold else "black",
+              size=text_size)
+
+  # Save the figure to the current working directory
+  if savefig:
+    fig.savefig("confusion_matrix.png")
+  
+# Make a function to predict on images and plot them (works with multi-class)
+def pred_and_plot(model, filename, class_names):
   """
-  :params: 
-         model - it contain the model in which we want to plot decision boundaries
-         x: input
-         y: input
-   :return:
-          it's return the decision boundaries
+  Imports an image located at filename, makes a prediction on it with
+  a trained model and plots the image with the predicted class as the title.
   """
+  # Import the target image and preprocess it
+  img = load_and_prep_image(filename)
 
-  # make meshgrid function for different values of x
-  x_min, x_max = x[:, 0] - 0.1, x[:, 1] + 0.1
-  y_min, y_max = x[:, 0] - 0.1 , x[:, 1] + 0.1
-
-  xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100), np.linspace(y_min, y_max, 100))
-
-
-  # Creating x values (which we use for prediction)
-  x_in = np.c_[xx.ravel(), yy.ravel()]
-
-  # Make prediction
-  y_pred = model.predict(x_in)
-
-  # Creating statement for multiclass
-  if len(y_pred[0]) > 0:
-    print("Dealing with Multiclass problem")
-    y_pred = np.argmax(y_pred, axis = 1).reshape(xx.shape)
-  else:
-    print("Dealing with binaryclass problem")
-    y_pred = np.round(y_pred).reshape(xx.shape) 
-
-    # Ploting all things together for visualize
-  plt.contourf(xx, yy, cmap=plt.cm.RdYlBu, alpha= 0.7) 
-  plt.scatter(x[:, 0], x[:, 1], c="y", s=40, cmap=plt.cm.RdYlBu)
-  plt.xlim(xx.min(), xx.max())
-  plt.ylim(yy.min(), yy.max())
-  
-  
-  # Function - 3 vew random omage
-  def plot_random_image(model, image, true_labels, classes ):
-  """
-  :params:
-         model - contain the model which we use to prediction
-         image - contain the sample which is use find that our model predictio is true or not
-         true_labels - true labels of our imae
-         classes - Contain the list of names as classes
-  
-  """
-  # Set up a random integer
-  i =random.randint(0, len(image))
-
-
-  # Create prediction and target
-  target_image = image[i]
-  # here we need a prediction probability
-
-  probs_pred = model.predict(target_image.reshape(1, 28, 28))
-  pred_labels = classes[probs_pred.argmax()]
-  true_label = classes[true_labels[i]]
-
-  # Plot the image
-  plt.figure(figsize=(10,10)) 
-  plt.imshow(target_image, cmap = plt.cm.binary)
-
-  # compare our prediction with actual data with our model prediction
-  if pred_labels == true_label:
-    color = "green"
-  else:
-    color = "red"  
-
-    # Add xlabel information(prediction/true_labels)
-   
-  plt.xlabel(" Pred :{} {:2.0f}% (True: {} ".format(pred_labels,
-                                                       100*tf.reduce_max(probs_pred),
-                                                       true_label),
-                                                       color=color)
-# function - 4  view random image using OS or directories
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import random
-import os
-
-def random_image_view(target_dir, target_class):
-
-  '''
-  :params:
-          target_dir - contain the target directories from which we want to view images
-          target_class - contain the class which we need to target as to view image
-   :return:
-          return a random image from our targeted_dir or from our targeted_class
-  '''
-  folder_path = target_dir+target_class
-
-  # getting random image from 
-  random_image = random.sample(os.listdir(folder_path), 1)
-  print(random_image)
-
-  # read and plot our random image
-  img = mpimg.imread(folder_path + "/" + random_image[0])
-  print(img.shape)
-  plt.imshow(img)
-  plt.title(target_class)
-  plt.axis("off");
-
-  return img
-
-
-# function - 5 plot loss curves
-def plot_loss_curve(history):
-  '''
-  :params:
-         contain model history to find the loss curves
-  :return:
-          plot loss and accuracy curves of given model history
-  '''
-  epochs= range(len(history.history["loss"]))
-  # loss
-  loss = history.history["loss"]
-  valid_loss = history.history["val_loss"]
-  
-  # Accuracy
-  accuracy = history.history["accuracy"]
-  valid_accuracy = history.history["val_accuracy"]
-
-  # loss curve
-  plt.figure(figsize=(8,5))
-  plt.plot(epochs, loss, label="Training_loss")
-  plt.plot(epochs, valid_loss, label="Testing_loss")
-  plt.xlabel('epochs')
-  plt.title("Loss_curves")
-  plt.legend()
-
-  # Accuracy curve
-  plt.figure(figsize=(8,5))
-  plt.plot(epochs, accuracy, label="Training_accuracy")
-  plt.plot(epochs, valid_accuracy, label="Testing_accuracy")
-  plt.xlabel('epochs')
-  plt.title("Accuracy_curve")
-  plt.legend();
-  
-  # Function 6 load and resize image according to our need
-  def load_resize_image(filename, img_shape=224):
-  '''
-    :params:
-            filename - it contain the filename which we want to resize
-            image_shape - this was the targeted size of image
-    :return:
-            return the resized image
-  '''
-  # Read image from filename
-  img = tf.io.read_file(filename)
-  
-  # Decode the read file into a tensor
-  img = tf.image.decode_image(img)
-
-  # resize the decoded img
-  img = tf.image.resize(img, size=[img_shape, img_shape])
-
-  # Rescale our image/normalize our image
-  img = img/255.
-
-  # Return our imag as output
-  return img
-
-
-# Function 7 - pred and plot
-def pred_and_plot(model, filename, class_name):
-  '''
-  :params:
-         model - it contain the model number which we use for prediction and plotting
-         filename - Containg the filename on which we make prediction
-         class_name - contain the classes we have for our experiments
-   :return:
-          return the image with prediction
-  '''
-  # load and preprocess our image
-  img = load_resize_image(filename)
-
-  # make prediction on our custom data
-  
+  # Make a prediction
   pred = model.predict(tf.expand_dims(img, axis=0))
 
-  if len(pred[0])>1:
-    pred_class = class_names[tf.argmax(pred[0])]
+  # Get the predicted class
+  if len(pred[0]) > 1: # check for multi-class
+    pred_class = class_names[pred.argmax()] # if more than one output, take the max
   else:
-    pred_class = class_name[int(tf.round(pred))]
-  #plot the image  our title
+    pred_class = class_names[int(tf.round(pred)[0][0])] # if only one output, round
+
+  # Plot the image and predicted class
   plt.imshow(img)
   plt.title(f"Prediction: {pred_class}")
   plt.axis(False);
   
-  
-  # function - 8 compare history by plots
-  def compare_hist(original_hist, new_hist, initial_epochs=5):
+import datetime
 
+def create_tensorboard_callback(dir_name, experiment_name):
   """
-    :params:
-      original_hist = it contain the history of model before fine tuning
-      new_hist = it contain the history of model after fine tuning
-      intial_epochs = how many epochs the model is use to run
-    :return:
-            a compare history of before and after fine_tuning  
+  Creates a TensorBoard callback instand to store log files.
+
+  Stores log files with the filepath:
+    "dir_name/experiment_name/current_datetime/"
+
+  Args:
+    dir_name: target directory to store TensorBoard log files
+    experiment_name: name of experiment directory (e.g. efficientnet_model_1)
+  """
+  log_dir = dir_name + "/" + experiment_name + "/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+  tensorboard_callback = tf.keras.callbacks.TensorBoard(
+      log_dir=log_dir
+  )
+  print(f"Saving TensorBoard log files to: {log_dir}")
+  return tensorboard_callback
+
+# Plot the validation and training data separately
+import matplotlib.pyplot as plt
+
+def plot_loss_curves(history):
+  """
+  Returns separate loss curves for training and validation metrics.
+
+  Args:
+    history: TensorFlow model History object (see: https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/History)
+  """ 
+  loss = history.history['loss']
+  val_loss = history.history['val_loss']
+
+  accuracy = history.history['accuracy']
+  val_accuracy = history.history['val_accuracy']
+
+  epochs = range(len(history.history['loss']))
+
+  # Plot loss
+  plt.plot(epochs, loss, label='training_loss')
+  plt.plot(epochs, val_loss, label='val_loss')
+  plt.title('Loss')
+  plt.xlabel('Epochs')
+  plt.legend()
+
+  # Plot accuracy
+  plt.figure()
+  plt.plot(epochs, accuracy, label='training_accuracy')
+  plt.plot(epochs, val_accuracy, label='val_accuracy')
+  plt.title('Accuracy')
+  plt.xlabel('Epochs')
+  plt.legend();
+
+def compare_historys(original_history, new_history, initial_epochs=5):
     """
-    # Accuracy and loss before fine tuning
-  acc = original_hist.history["accuracy"]
-  loss = original_hist.history["loss"]
+    Compares two TensorFlow model History objects.
+    
+    Args:
+      original_history: History object from original model (before new_history)
+      new_history: History object from continued model training (after original_history)
+      initial_epochs: Number of epochs in original_history (new_history plot starts from here) 
+    """
+    
+    # Get original history measurements
+    acc = original_history.history["accuracy"]
+    loss = original_history.history["loss"]
 
-  val_acc = original_hist.history["val_accuracy"]
-  val_loss = original_hist.history["val_loss"]
+    val_acc = original_history.history["val_accuracy"]
+    val_loss = original_history.history["val_loss"]
 
-  # Accuracy and loss before fine tuning
-  total_acc = acc + new_hist.history["accuracy"]
-  total_loss = loss + new_hist.history["loss"]
+    # Combine original history with new history
+    total_acc = acc + new_history.history["accuracy"]
+    total_loss = loss + new_history.history["loss"]
 
-  total_val_acc = val_acc + new_hist.history["val_accuracy"]
-  total_val_loss = val_loss + new_hist.history["val_loss"]
+    total_val_acc = val_acc + new_history.history["val_accuracy"]
+    total_val_loss = val_loss + new_history.history["val_loss"]
 
-  # plot the compared loss curves of our models
-  plt.figure(figsize= (8, 8))
-  plt.subplot(2, 1, 1)
-  plt.plot(total_acc, label="Training_accuracy")
-  plt.plot(total_val_acc, label = "Val_accuracy")
-  plt.plot([initial_epochs-1, initial_epochs-1], plt.ylim(), label = 'Start fine tuning')
-  plt.legend(loc = "lower right")
-  plt.title("Training and Validation accuracy")
-  # plot of losses 
-  plt.subplot(2, 1, 2)
-  plt.plot(total_loss, label="Training_accurloss")
-  plt.plot(total_val_loss, label = "Val_accurloss")
-  plt.plot([initial_epochs-1, initial_epochs-1], plt.ylim(), label = 'Start fine tuning')
-  plt.legend(loc = "upper right")
-  plt.title("Training and Validation loss")
+    # Make plots
+    plt.figure(figsize=(8, 8))
+    plt.subplot(2, 1, 1)
+    plt.plot(total_acc, label='Training Accuracy')
+    plt.plot(total_val_acc, label='Validation Accuracy')
+    plt.plot([initial_epochs-1, initial_epochs-1],
+              plt.ylim(), label='Start Fine Tuning') # reshift plot around epochs
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(2, 1, 2)
+    plt.plot(total_loss, label='Training Loss')
+    plt.plot(total_val_loss, label='Validation Loss')
+    plt.plot([initial_epochs-1, initial_epochs-1],
+              plt.ylim(), label='Start Fine Tuning') # reshift plot around epochs
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('epoch')
+    plt.show()
+  
+# Create function to unzip a zipfile into current working directory 
+# (since we're going to be downloading and unzipping a few files)
+import zipfile
+
+def unzip_data(filename):
+  """
+  Unzips filename into the current working directory.
+
+  Args:
+    filename (str): a filepath to a target zip folder to be unzipped.
+  """
+  zip_ref = zipfile.ZipFile(filename, "r")
+  zip_ref.extractall()
+  zip_ref.close()
+
+# Walk through an image classification directory and find out how many files (images)
+# are in each subdirectory.
+import os
+
+def walk_through_dir(dir_path):
+  """
+  Walks through dir_path returning its contents.
+
+  Args:
+    dir_path (str): target directory
+  
+  Returns:
+    A print out of:
+      number of subdiretories in dir_path
+      number of images (files) in each subdirectory
+      name of each subdirectory
+  """
+  for dirpath, dirnames, filenames in os.walk(dir_path):
+    print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
+    
+# Function to evaluate: accuracy, precision, recall, f1-score
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+
+def calculate_results(y_true, y_pred):
+  """
+  Calculates model accuracy, precision, recall and f1 score of a binary classification model.
+
+  Args:
+      y_true: true labels in the form of a 1D array
+      y_pred: predicted labels in the form of a 1D array
+
+  Returns a dictionary of accuracy, precision, recall, f1-score.
+  """
+  # Calculate model accuracy
+  model_accuracy = accuracy_score(y_true, y_pred) * 100
+  # Calculate model precision, recall and f1 score using "weighted average
+  model_precision, model_recall, model_f1, _ = precision_recall_fscore_support(y_true, y_pred, average="weighted")
+  model_results = {"accuracy": model_accuracy,
+                  "precision": model_precision,
+                  "recall": model_recall,
+                  "f1": model_f1}
+  return model_results
+
